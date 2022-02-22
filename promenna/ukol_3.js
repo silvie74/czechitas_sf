b2b2a3728cb92cb550aed32e347491e0818a1daa
silvie74 <s.flidrova@seznam.cz>
2022-02-22 20:03:35 +0100

    1 file modified

--- promenna/ukol_3.js
+++ promenna/ukol_3.js
@@ -2,7 +2,17 @@ let a = Number(prompt("Zadej číslo A:"));
 let b = Number(prompt("Zadej číslo B:"));
 let c = Number(prompt("Zadej číslo C:"));
 
-if (a > b) {if (a > c) console.log("Největší číslo je číslo A:" + a)
- } else console.log("Největší číslo je číslo C:" + c) }} else {if (c<b) console.log("Největší je číslo B:" + b)} else {console.log("Největší je číslo C:" + c)}
+if (a > b) {
+  if (a > c) {console.log("Největší číslo je číslo A:" + a);
+ } else {
+   console.log("Největší číslo je číslo C:" + c);
+   }
+  } else {
+    if (c<b) {
+      console.log("Největší je číslo B:" + b);
+    } else {
+      console.log("Největší je číslo C:" + c);
+    }
+  }
 
   
