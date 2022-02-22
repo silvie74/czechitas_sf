cdea9000388024c0941d7bebf4b34a322c061f54
silvie74 <s.flidrova@seznam.cz>
2022-02-22 19:53:53 +0100

    1 file modified

--- promenna/ukol_3.js
+++ promenna/ukol_3.js
@@ -3,6 +3,6 @@ let b = Number(prompt("Zadej číslo B:"));
 let c = Number(prompt("Zadej číslo C:"));
 
 if (a > b) {if (a > c) console.log("Největší číslo je číslo A:" + a)
- } else console.log("Největší číslo je číslo C:" + c) } else {if (c<b) console.log("Největší je číslo B:" + b)} else {console.log("Největší je číslo C:" + c)}
+ } else console.log("Největší číslo je číslo C:" + c) }} else {if (c<b) console.log("Největší je číslo B:" + b)} else {console.log("Největší je číslo C:" + c)}
 
   
