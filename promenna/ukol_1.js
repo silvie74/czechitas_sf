ee107ade9ab1dd7cd25582af7fc97afee0764c7a
silvie74 <s.flidrova@seznam.cz>
2022-02-22 18:21:52 +0100

    1 file modified, 1 added

---
+++ promenna/ukol_1.js
@@ -0,0 +1,7 @@
+let jmeno = "Silvie";
+let vek = 47;
+let vaha = 56;
+let vyska = 158;
+let bmi = vaha / (vyska * vyska)
+
+console.log ("Jmenuji se " + jmeno + " a je mi " + vek + " let, vážím " + vaha + " kg, měřím " + vyska * 0.01 + " m a moje BMI je " + bmi)
