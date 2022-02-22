6bf64d2a6d29a930a0ddbd87d15340bb07394597
silvie74 <s.flidrova@seznam.cz>
2022-02-22 19:06:01 +0100

    1 file modified

--- promenna/ukol_1.js
+++ promenna/ukol_1.js
@@ -1,7 +1,9 @@
-let jmeno = "Silvie";
-let vek = 47;
-let vaha = 56;
-let vyska = 158;
-let bmi = vaha / (vyska * vyska)
+let jmeno = prompt("Jak se jmenuješ?");
+let vek = Number(prompt("Kolik je ti let?"));
+let vaha = Number(prompt("kolik kg vážíš:"));
+let vyska = Number(prompt("Kolik cm měříš?"));
+let bmi = vaha / (vyska * vyska * 0.0001)
 
 console.log ("Jmenuji se " + jmeno + " a je mi " + vek + " let, vážím " + vaha + " kg, měřím " + vyska * 0.01 + " m a moje BMI je " + bmi)
+
+if (vek >= 18) {conzole.log("Výborně, jsi plnoletý!")} else {console.log("Bohužel, nejsi plnoletý")}
